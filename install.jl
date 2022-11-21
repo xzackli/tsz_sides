665e99fc115c0df25d8f745cc07124d6059b874c
xzackli <xzackli@gmail.com>
2022-11-22 04:53:21 +0900

    remove extraneous cosmo and add params

--- install.jl
+++ install.jl
@@ -2,7 +2,7 @@ using Pkg, Downloads
 
 Pkg.pkg"add WCS HDF5 Interpolations Cosmology JSON JLD2"
 Pkg.pkg"add https://github.com/simonsobs/Pixell.jl.git"
-Pkg.pkg"add https://github.com/xzackli/XGPaint.jl.git#prof"
+Pkg.pkg"add https://github.com/xzackli/XGPaint.jl.git#main"
 
 
 Downloads.download("http://www.cita.utoronto.ca/~zack/cached_battaglia.jld2", 
